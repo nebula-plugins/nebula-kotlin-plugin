e8cda4b09657ab0dbade755a71c8bef603f173c8
Danny Thomas <dannyt@netflix.com>
2016-07-14 13:53:27 -0700

    Convert the plugin to Kotlin

---
+++ src/main/kotlin/netflix/nebula/plugin.kt
@@ -0,0 +1,39 @@
+package netflix.nebula
+
+import org.gradle.api.Plugin
+import org.gradle.api.Project
+import org.jetbrains.kotlin.gradle.plugin.KotlinPlugin
+import java.io.FileNotFoundException
+import java.util.*
+
+class NebulaKotlinPlugin : Plugin<Project> {
+    override fun apply(project: Project) {
+        kotlin.with(project) {
+            plugins.apply("kotlin")
+
+            val kotlinVersion = loadKotlinVersion()
+
+            dependencies.add("compile", "org.jetbrains.kotlin:kotlin-stdlib:$kotlinVersion")
+
+            configurations.all({ configuration ->
+                configuration.resolutionStrategy.eachDependency { details ->
+                    val requested = details.requested
+                    if (requested.group.equals("org.jetbrains.kotlin") && requested.version.isEmpty()) {
+                        details.useTarget("${requested.group}:${requested.name}:$kotlinVersion")
+                    }
+                }
+            })
+        }
+    }
+
+    fun loadKotlinVersion(): String {
+        val props = Properties()
+        val propFileName = "project.properties"
+        val inputStream = KotlinPlugin::class.java.classLoader.getResourceAsStream(propFileName) ?: throw FileNotFoundException("property file '$propFileName' not found in the classpath")
+
+        props.load(inputStream)
+
+        val projectVersion = props ["project.version"] as String
+        return projectVersion
+    }
+}
